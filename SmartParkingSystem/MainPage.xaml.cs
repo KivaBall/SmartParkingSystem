e0003c3abe724f8b5db54ed3e1e4e866fb5beecb
Ihor Kobzar <supermensy@gmail.com>
2026-03-31 21:17:47 +0300

    Initial commit

---
+++ SmartParkingSystem/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartParkingSystem;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SmartParkingSystem/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SmartParkingSystem;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
