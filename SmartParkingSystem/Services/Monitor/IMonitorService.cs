28feea8b547bd25ae30237aee7efa8dc033f57cb
Ihor Kobzar <supermensy@gmail.com>
2026-04-08 21:37:27 +0300

    feat: add monitor service foundation

---
+++ SmartParkingSystem/Services/Monitor/IMonitorService.cs
@@ -0,0 +1,11 @@
+﻿using SmartParkingSystem.Models.Monitor;
+
+namespace SmartParkingSystem.Services.Monitor;
+
+public interface IMonitorService
+{
+    Task<MonitorSnapshot> GetSnapshotAsync(CancellationToken cancellationToken = default);
+    Task<MonitorSnapshot> RefreshAsync(CancellationToken cancellationToken = default);
+    Task<MonitorSnapshot> SaveAsync(MonitorEditableSettings settings, CancellationToken cancellationToken = default);
+    Task<MonitorSnapshot> ResetAsync(CancellationToken cancellationToken = default);
+}
